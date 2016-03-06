300b9b0e20792d23e6fa1c6d2f8da9f495116151
jianlong108 <wlong108@iCloud.com>
2016-03-06 18:02:48 +0800

    增加对图片选择后的使用...

--- IPickerDemo/IPickerDemo/ViewController.m
+++ IPickerDemo/IPickerDemo/ViewController.m
@@ -13,37 +13,64 @@
 @interface ViewController ()<IPickerViewControllerDelegate>
 /**sdf*/
 @property (nonatomic, strong)NSArray *arr;
+
+/**d*/
+@property (nonatomic, strong)UIImageView *img1;
+@property (nonatomic, strong)UIImageView *img2;
+@property (nonatomic, strong)UIImageView *img3;
+@property (nonatomic, strong)UIImageView *img4;
+
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    _img1 = [[UIImageView alloc]initWithFrame:CGRectMake(0, 50, 100, 100)];
+    [self.view addSubview:_img1];
+    _img2 = [[UIImageView alloc]initWithFrame:CGRectMake(130, 50, 100, 100)];
+    [self.view addSubview:_img2];
+    _img3 = [[UIImageView alloc]initWithFrame:CGRectMake(0, 170, 100, 100)];
+    [self.view addSubview:_img3];
+    _img4 = [[UIImageView alloc]initWithFrame:CGRectMake(130, 170, 100, 100)];
+    [self.view addSubview:_img4];
+    
 }
-
+- (IBAction)popIPicker:(UIButton *)sender{
+    IPickerViewController *ip = [[IPickerViewController alloc]init];
+    ip.delegate = self;
+    ip.maxCount = 11;
+    ip.popStyle = IPickerViewControllerPopStylePush;
+    [self.navigationController pushViewController:ip animated:YES];
+    //    [self presentViewController:ip animated:YES completion:nil];
+    
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-- (IBAction)test:(UIButton *)sender {
-    NSLog(@"%@",self.arr);
-}
-- (IBAction)entryImagePicker {
-    IPickerViewController *vc = [[IPickerViewController alloc]init];
-    vc.delegate = self;
-    vc.maxCount = 20;
-    if (self.navigationController) {
-        [self.navigationController pushViewController:vc
-                                             animated:YES];
-    }else {
-        [self presentViewController:vc animated:YES completion:nil];
-    }
-    
-}
-#pragma mark IPickerViewControllerDelegate
 - (void)didClickCompleteBtn:(NSArray *)datas{
-    self.arr = datas;
     NSLog(@"%@",datas);
+    
+    [datas enumerateObjectsUsingBlock:^(IPImageModel * obj, NSUInteger idx, BOOL * _Nonnull stop) {
+        if (idx == 0) {
+            [IPickerViewController getImageModelWithURL:obj.assetUrl CreatBlock:^(IPImageModel *obg) {
+                _img1.image = obg.thumbnail;
+            }];
+        }else if (idx == 1){
+            [IPickerViewController getImageModelWithURL:obj.assetUrl CreatBlock:^(IPImageModel *obg) {
+                _img2.image = obg.thumbnail;
+            }];
+        }else if (idx == 2){
+            [IPickerViewController getImageModelWithURL:obj.assetUrl CreatBlock:^(IPImageModel *obg) {
+                _img3.image = obg.fullRorationImage;
+            }];
+        }else if (idx == 3){
+            [IPickerViewController getImageModelWithURL:obj.assetUrl CreatBlock:^(IPImageModel *obg) {
+                _img4.image = obg.fullRorationImage;
+            }];
+        }
+    }];
 }
 @end
